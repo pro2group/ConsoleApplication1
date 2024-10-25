3a9884c4e40ea776770438c12c07cc771e60c2ab
drugsnotnx <kuzma2005steam@mail.ru>
2024-10-26 03:48:20 +0500

    зафиксировал акермана)

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -5,7 +5,18 @@ int fib(int n) {
         return n;
     return fib(n - 1) + fib(n - 2);
 }
+int ackermann(int m, int n) {
+    if (m == 0) return n + 1;
+    if (m == 1) return n + 2;
+    if (m == 2) return 2 * n + 3;
+    if (m == 3) return (1 << (n + 3)) - 3;
+    return ackermann(m - 1, ackermann(m, n - 1));
+}
 int main() {
     int n = 5;
     cout << fib(n);
+    int a, b;
+    cin >> a >> b;
+    cout << ackermann(a, b) << endl;
+
 }
